464533f9dbdec7d6002cc17d7312299c70b3158b
Steve Gray <heystevegray.dev@gmail.com>
2025-04-26 14:39:39 -0400

    Lid

--- app/components/shows/severance.tsx
+++ app/components/shows/severance.tsx
@@ -18,7 +18,7 @@ const Numbers = () => {
   return (
     <div className='flex flex-row items-center justify-center gap-2 flex-wrap'>
       {randomNumbers.map((number, index) => (
-        <div key={index} className='size-8 flex items-center justify-center font-bold hover:font-normal hover:text-6xl transition-all duration-300 ease-in-out severance-number'>
+        <div key={index} className='size-8 flex border-red-500 border items-center justify-center font-bold hover:font-normal hover:text-6xl transition-all duration-300 ease-in-out'>
           {number}
         </div>
       ))}
@@ -32,7 +32,8 @@ const BoxLid = ({ open }: { open: boolean }) => {
   }
 
   return (
-    <div>
+    <div className='absolute top-0 left-0 right-0'>
+      <div className='absolute -top-4 left-0 right-0 border-cyan-100 border-2 h-4 bg-slate-950' />
       <div className='absolute -top-9 -skew-x-[136deg] -translate-x-1/2 -left-[22px] rotate-45 border-cyan-100 h-3 w-16 border-l-0 border-4' />
       <div className='absolute -top-9 skew-x-[136deg] translate-x-1/2 -right-[22px] -rotate-45 border-cyan-100 h-3 w-16 border-r-0 border-4' />
     </div>
@@ -44,14 +45,7 @@ const Bin = ({ number, initialOpen = false }: { number: number; initialOpen?: bo
   const completion = getRandomNumberBetween(0, 100)
   return (
     <div className='grid grid-cols-1 grid-rows-2 gap-1 relative'>
-      {open ? (
-        <div className='absolute top-0 left-0 right-0'>
-          {/* Top */}
-          <div className='absolute -top-4 left-0 right-0 border-cyan-100 border-2 h-4 bg-slate-950' />
-          <BoxLid open={open} />
-          <BoxLid open={open} />
-        </div>
-      ) : null}
+      {open ? <BoxLid open={open} /> : null}
       <div className='text-lg border-cyan-500 border-2 flex justify-center items-center'>{String(number).padStart(2, '0')}</div>
       <div
         className='text-lg border-cyan-500 border-2 flex justify-center items-center'
@@ -67,7 +61,7 @@ const Bin = ({ number, initialOpen = false }: { number: number; initialOpen?: bo
 
 const Severance = () => {
   return (
-    <div className='min-h-dvh w-full bg-slate-950 text-cyan-500 grid grid-rows-[auto_1fr_auto]'>
+    <div className='h-dvh w-full bg-slate-950 text-cyan-500 grid grid-rows-[auto_1fr_auto]'>
       <div className='p-8'>
         <header className='flex items-center justify-between h-16 border-2 border-cyan-500 p-2 max-w-3xl mx-auto'>
           <h1 className='text-2xl font-bold'>Dranesville</h1>
